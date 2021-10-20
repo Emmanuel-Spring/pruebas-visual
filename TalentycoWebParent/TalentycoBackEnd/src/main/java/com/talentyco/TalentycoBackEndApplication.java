2541a92ed25018b571341b31d722ec4bd5ef21f1
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-19 22:10:59 -0300

    Talentyco Config

---
+++ TalentycoWebParent/TalentycoBackEnd/src/main/java/com/talentyco/TalentycoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentycoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentycoBackEndApplication.class, args);
+	}
+
+}
---
+++ TalentycoWebParent/TalentycoFrontEnd/src/main/java/com/talentyco/TalentycoFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentycoFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentycoFrontEndApplication.class, args);
+	}
+
+}
